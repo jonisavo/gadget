d685058212ed18ea68bd62dfc7af4fdf3e5c2ec8
Joni Savolainen <joni@savolainen.io>
2022-03-27 17:38:35 +0300

    Add DisabledAttribute

---
+++ Core/DisabledAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace InspectorEssentials.Core
+{
+    /// <summary>
+    /// When used on a field, it is marked as disabled in the inspector.
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// [SerializeField] [Disabled]
+    /// private Vector3 _currentPosition;
+    /// </code>
+    /// </example>
+    public class DisabledAttribute : BasePropertyAttribute {}
+}
---
+++ Editor/DrawerExtensions/DisabledDrawerExtension.cs
@@ -0,0 +1,15 @@
+﻿using InspectorEssentials.Core;
+using UnityEditor;
+
+namespace InspectorEssentials.Editor.DrawerExtensions
+{
+    public class DisabledDrawerExtension : PropertyDrawerExtension<DisabledAttribute>
+    {
+        public DisabledDrawerExtension(BasePropertyAttribute attribute) : base(attribute) {}
+
+        public override bool IsEnabled(SerializedProperty property)
+        {
+            return false;
+        }
+    }
+}
