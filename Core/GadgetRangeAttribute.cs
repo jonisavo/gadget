e917fd07e2c6be30c0e0ea6d0ddb50dfa8b03bb2
Joni Savolainen <joni@savolainen.io>
2022-03-27 22:07:05 +0300

    Add GadgetRangeAttribute

---
+++ Core/GadgetRangeAttribute.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Gadget.Core
+{
+    /// <summary>
+    /// A version of <see cref="RangeAttribute"/> that works with other
+    /// <see cref="GadgetPropertyAttribute"/> type Attributes.
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// [GadgetRange(0f, 2f), Disabled]
+    /// public float disabledRange;
+    /// </code>
+    /// </example>
+    public class GadgetRangeAttribute : GadgetPropertyAttribute
+    {
+        public readonly float Min;
+        public readonly float Max;
+
+        public GadgetRangeAttribute(float min, float max)
+        {
+            Min = min;
+            Max = max;
+        }
+    }
+}
---
+++ Editor/DrawerExtensions/GadgetRangeDrawerExtension.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using Gadget.Core;
+using UnityEditor;
+
+namespace Gadget.Editor.DrawerExtensions
+{
+    public class GadgetRangeDrawerExtension : PropertyDrawerExtension<GadgetRangeAttribute>
+    {
+        public GadgetRangeDrawerExtension(GadgetPropertyAttribute attribute) : base(attribute) {}
+
+        private GadgetRangeAttribute RangeAttribute
+        {
+            get
+            {
+                if (_rangeAttribute == null)
+                    _rangeAttribute = (GadgetRangeAttribute) Attribute;
+
+                return _rangeAttribute;
+            }
+        }
+
+        private GadgetRangeAttribute _rangeAttribute;
+
+        public override bool TryOverrideMainGUI(DrawerExtensionCallbackInfo info)
+        {
+            if (!IsPropertyValid(info.Property))
+                return false;
+            
+            switch (info.Property.propertyType)
+            {
+                case SerializedPropertyType.Integer:
+                    EditorGUI.IntSlider(info.Position, info.Property,
+                        (int) RangeAttribute.Min, (int) RangeAttribute.Max, info.Content);
+                    break;
+                case SerializedPropertyType.Float:
+                    EditorGUI.Slider(info.Position, info.Property,
+                        RangeAttribute.Min, RangeAttribute.Max);
+                    break;
+                default:
+                    return false;
+            }
+
+            return true;
+        }
+
+        public override bool IsInvalid(SerializedProperty property, FieldInfo fieldInfo, out string errorMessage)
+        {
+            errorMessage = $"Field {fieldInfo.Name} is not an integer or a float";
+            return !IsPropertyValid(property);
+        }
+
+        private static bool IsPropertyValid(SerializedProperty property)
+        {
+            return property.propertyType == SerializedPropertyType.Integer ||
+                   property.propertyType == SerializedPropertyType.Float;
+        }
+    }
+}
