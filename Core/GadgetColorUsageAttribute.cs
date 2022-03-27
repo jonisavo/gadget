d1b64e554ff6c2f4eaa640406fc9e798c44cd284
Joni Savolainen <joni@savolainen.io>
2022-03-27 23:18:09 +0300

    Add GadgetColorUsageAttribute

---
+++ Core/GadgetColorUsageAttribute.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Gadget.Core
+{
+    /// <summary>
+    /// A version of <see cref="ColorUsageAttribute"/> that works with other
+    /// <see cref="GadgetPropertyAttribute"/> type Attributes.
+    /// Has an additional property <c>ShowEyedropper</c> which controls
+    /// whether the eye dropper button should be visible.
+    /// Has additional <c>ShowAlpha</c> and <c>HDR</c> properties for
+    /// configuring the attribute without boolean arguments.
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// [GadgetColorUsage(true, true)]
+    /// public Color color;
+    /// 
+    /// [GadgetColorUsage(ShowAlpha = false, HDR = true, ShowEyedropper = false)]
+    /// public Color anotherColor;
+    /// </code>
+    /// </example>
+    public class GadgetColorUsageAttribute : GadgetPropertyAttribute
+    {
+        public bool ShowAlpha { get; set; }
+
+        public bool ShowEyedropper { get; set; } = true;
+        
+        public bool HDR { get; set; }
+
+        public GadgetColorUsageAttribute() {}
+
+        public GadgetColorUsageAttribute(bool showAlpha)
+        {
+            ShowAlpha = showAlpha;
+        }
+
+        public GadgetColorUsageAttribute(bool showAlpha, bool hdr)
+        {
+            ShowAlpha = showAlpha;
+            HDR = hdr;
+        }
+    }
+}
---
+++ Editor/DrawerExtensions/GadgetColorUsageDrawerExtension.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using Gadget.Core;
+using UnityEditor;
+
+namespace Gadget.Editor.DrawerExtensions
+{
+    public class GadgetColorUsageDrawerExtension : PropertyDrawerExtension<GadgetColorUsageAttribute>
+    {
+        public GadgetColorUsageDrawerExtension(GadgetPropertyAttribute attribute) : base(attribute) {}
+
+        public override bool TryOverrideMainGUI(DrawerExtensionCallbackInfo info)
+        {
+            if (IsInvalid(info.Property, info.FieldInfo, out _))
+                return false;
+
+            var colorUsageAttribute = (GadgetColorUsageAttribute) Attribute;
+            
+            EditorGUI.BeginChangeCheck();
+
+            var color = EditorGUI.ColorField(info.Position, info.Content, info.Property.colorValue,
+                colorUsageAttribute.ShowEyedropper, colorUsageAttribute.ShowAlpha,
+                colorUsageAttribute.HDR);
+
+            if (EditorGUI.EndChangeCheck())
+                info.Property.colorValue = color;
+
+            return true;
+        }
+
+        public override bool IsInvalid(SerializedProperty property, FieldInfo fieldInfo, out string errorMessage)
+        {
+            errorMessage = $"Field {fieldInfo.Name} is not a Color.";
+            return property.propertyType != SerializedPropertyType.Color;
+        }
+    }
+}
