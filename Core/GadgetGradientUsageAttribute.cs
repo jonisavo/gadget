feb68b612363cc0f5da997e183d5006db7c17b3e
Joni Savolainen <joni@savolainen.io>
2022-03-28 09:56:11 +0300

    Add GadgetGradientUsageAttribute

---
+++ Core/GadgetGradientUsageAttribute.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Gadget.Core
+{
+    /// <summary>
+    /// A version of <see cref="GradientUsageAttribute"/> that works with other
+    /// <see cref="GadgetPropertyAttribute"/> type Attributes.
+    /// Has additional <c>ColorSpace</c> and <c>HDR</c> properties for
+    /// configuring the attribute without boolean arguments.
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// [GadgetGradientUsage(ColorSpace.Linear, true)]
+    /// public Gradient gradient;
+    /// 
+    /// [GadgetGradientUsage(ColorSpace = ColorSpace.Linear, HDR = true)]
+    /// public Gradient anotherGradient;
+    /// </code>
+    /// </example>
+    public class GadgetGradientUsageAttribute : GadgetPropertyAttribute
+    {
+        public ColorSpace ColorSpace { get; set; }
+
+        public bool HDR { get; set; }
+
+        public GadgetGradientUsageAttribute() {}
+
+        public GadgetGradientUsageAttribute(bool hdr)
+        {
+            HDR = hdr;
+        }
+
+        public GadgetGradientUsageAttribute(bool hdr, ColorSpace colorSpace)
+        {
+            HDR = hdr;
+            ColorSpace = colorSpace;
+        }
+    }
+}
---
+++ Editor/DrawerExtensions/GadgetGradientUsageDrawerExtension.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using Gadget.Core;
+using UnityEditor;
+using UnityEngine;
+
+namespace Gadget.Editor.DrawerExtensions
+{
+    public class GadgetGradientUsageDrawerExtension : PropertyDrawerExtension<GadgetGradientUsageAttribute>
+    {
+        public GadgetGradientUsageDrawerExtension(GadgetPropertyAttribute attribute) : base(attribute) {}
+
+        public override bool TryOverrideMainGUI(DrawerExtensionCallbackInfo info)
+        {
+            if (IsInvalid(info.Property, info.FieldInfo, out _))
+                return false;
+
+            // gradientValue is internal for some strange reason.
+            var gradientPropertyInfo = info.Property.GetType().GetProperty("gradientValue",
+                BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (gradientPropertyInfo == null)
+                return false;
+            
+            var gradientUsageAttribute = (GadgetGradientUsageAttribute) Attribute;
+            
+            EditorGUI.BeginChangeCheck();
+
+            var currentGradient = gradientPropertyInfo.GetValue(info.Property, null) as Gradient;
+
+            var gradient = EditorGUI.GradientField(info.Position, info.Content, currentGradient,
+                gradientUsageAttribute.HDR, gradientUsageAttribute.ColorSpace);
+
+            if (EditorGUI.EndChangeCheck())
+                gradientPropertyInfo.SetValue(info.Property, gradient);
+
+            return true;
+        }
+
+        public override bool IsInvalid(SerializedProperty property, FieldInfo fieldInfo, out string errorMessage)
+        {
+            errorMessage = $"Field {fieldInfo.Name} is not a Gradient.";
+            return property.propertyType != SerializedPropertyType.Gradient;
+        }
+    }
+}
