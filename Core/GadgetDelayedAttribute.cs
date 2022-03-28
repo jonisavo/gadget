9a973bcd413f2512cd90af623e0e9390158df702
Joni Savolainen <joni@savolainen.io>
2022-03-28 10:24:45 +0300

    Add GadgetDelayedAttribute

---
+++ Core/GadgetDelayedAttribute.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Gadget.Core
+{
+    /// <summary>
+    /// A version of <see cref="DelayedAttribute"/> that works with other
+    /// <see cref="GadgetPropertyAttribute"/> type Attributes.
+    /// <example>
+    /// <code>
+    /// [GadgetDelayed]
+    /// public float value;
+    /// </code>
+    /// </example>
+    /// </summary>
+    public class GadgetDelayedAttribute : GadgetPropertyAttribute
+    {
+        
+    }
+}
---
+++ Editor/DrawerExtensions/GadgetDelayedDrawerExtension.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using Gadget.Core;
+using UnityEditor;
+
+namespace Gadget.Editor.DrawerExtensions
+{
+    public class GadgetDelayedDrawerExtension : PropertyDrawerExtension<GadgetDelayedAttribute>
+    {
+        public GadgetDelayedDrawerExtension(GadgetPropertyAttribute attribute) : base(attribute) {}
+
+        public override bool TryOverrideMainGUI(DrawerExtensionCallbackInfo info)
+        {
+            if (!IsPropertyValid(info.Property))
+                return false;
+
+            switch (info.Property.propertyType)
+            {
+                case SerializedPropertyType.Float:
+                    EditorGUI.DelayedFloatField(info.Position, info.Property, info.Content);
+                    break;
+                case SerializedPropertyType.Integer:
+                    EditorGUI.DelayedIntField(info.Position, info.Property, info.Content);
+                    break;
+                case SerializedPropertyType.String:
+                    EditorGUI.DelayedTextField(info.Position, info.Property, info.Content);
+                    break;
+                default:
+                    return false;
+            }
+
+            return true;
+        }
+
+        public override bool IsInvalid(SerializedProperty property, FieldInfo fieldInfo, out string errorMessage)
+        {
+            errorMessage = $"Field {fieldInfo.Name} is not a float, an integer or a string";
+            return !IsPropertyValid(property);
+        }
+
+        private static bool IsPropertyValid(SerializedProperty property)
+        {
+            return property.propertyType == SerializedPropertyType.Float ||
+                   property.propertyType == SerializedPropertyType.Integer ||
+                   property.propertyType == SerializedPropertyType.String;
+        }
+    }
+}
