6b5abc82aadef86e3d34190bff5bfe457c7c6bfd
Joni Savolainen <joni@savolainen.io>
2022-03-30 18:31:02 +0300

    Add some documentation for GadgetTooltipAttribute

--- Core/GadgetTooltipAttribute.cs
+++ Core/GadgetTooltipAttribute.cs
@@ -1,5 +1,11 @@
-﻿namespace Gadget.Core
+﻿using UnityEngine;
+
+namespace Gadget.Core
 {
+    /// <summary>
+    /// A version of <see cref="TooltipAttribute"/> that works with other
+    /// <see cref="GadgetPropertyAttribute"/> type Attributes.
+    /// </summary>
     public class GadgetTooltipAttribute : GadgetPropertyAttribute
     {
         public readonly string Text;
